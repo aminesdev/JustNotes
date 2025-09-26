082e05a51f87bd91fe0e04cba4e6b3598a12984b
aminesdev <aminesdev@proton.me>
2025-09-26 21:45:43 +0100

    Rate limiting

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,10 @@
 import express from "express";
 import routes from "./routes/index.js";
+import { apiLimiter } from "./middlewares/rateLimiter.js";
 
 const app = express();
 app.use(express.json());
 
-app.use("/api", routes);
+app.use("/api", apiLimiter, routes);
 
 export default app;
---
+++ backend/src/middlewares/rateLimiter.js
@@ -0,0 +1,14 @@
+import rateLimit from "express-rate-limit";
+import RedisStore from "rate-limit-redis";
+import redisClient from "../config/redis.js";
+
+export const apiLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 2,
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: { msg: "Too many requests, try again later." },
+    store: new RedisStore({
+        sendCommand: (...args) => redisClient.sendCommand(args),
+    }),
+});
