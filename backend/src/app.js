4f318946bb115d9093c8388b8723721b64fc42e5
aminesdev <aminesdev@proton.me>
2025-10-02 22:06:21 +0100

    fix cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ import cors from "cors";
 const app = express();
 app.use(
     cors({
-        origin: true,
+        origin: "https://just-notes-8z5w53bu6-aminesdevs-projects.vercel.app",
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization"],
